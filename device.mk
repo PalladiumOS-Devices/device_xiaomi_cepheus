f8bceb3f4f215c820722434103e3c160b9935e20
dark-free <emchaouki@gmail.com>
2022-03-21 16:28:36 +0000

    cepheus: bring palladium overlay

--- device.mk
+++ device.mk
@@ -398,7 +398,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-elixir
+    $(LOCAL_PATH)/overlay-palladium
 
 # Power
 PRODUCT_PACKAGES += \
